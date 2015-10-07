8d4439cdea1c76a5a46fed033f184b6186590a2c
erlingfjelstad <erlingfjelstad@gmail.com>
2015-10-07 11:14:14 +0200

    EnrollmentDateFragment now uses enrollmentDatePickers for selecting dates

--- app/src/main/java/org/hisp/dhis/android/trackercapture/fragments/enrollmentdate/EnrollmentDateFragment.java
+++ app/src/main/java/org/hisp/dhis/android/trackercapture/fragments/enrollmentdate/EnrollmentDateFragment.java
@@ -1,35 +1,23 @@
 package org.hisp.dhis.android.trackercapture.fragments.enrollmentdate;
 
-import android.app.DatePickerDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.content.Loader;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 import com.raizlabs.android.dbflow.structure.Model;
 import com.squareup.otto.Subscribe;
 
-import org.hisp.dhis.android.sdk.controllers.DhisController;
 import org.hisp.dhis.android.sdk.persistence.loaders.DbLoader;
-import org.hisp.dhis.android.sdk.ui.activities.OnBackPressedListener;
+import org.hisp.dhis.android.sdk.ui.adapters.rows.dataentry.Row;
 import org.hisp.dhis.android.sdk.ui.fragments.dataentry.DataEntryFragment;
 import org.hisp.dhis.android.sdk.ui.fragments.dataentry.RowValueChangedEvent;
 import org.hisp.dhis.android.sdk.ui.fragments.dataentry.SaveThread;
-import org.hisp.dhis.android.sdk.utils.UiUtils;
-import org.hisp.dhis.android.sdk.utils.support.DateUtils;
 import org.hisp.dhis.android.trackercapture.R;
-import org.joda.time.DateTime;
-import org.joda.time.LocalDate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,7 +27,7 @@ import java.util.List;
 /**
  * Created by erling on 7/16/15.
  */
-public class EnrollmentDateFragment extends DataEntryFragment<EnrollmentDateFragmentForm> implements OnBackPressedListener
+public class EnrollmentDateFragment extends DataEntryFragment<EnrollmentDateFragmentForm>
 {
     private static final String ENROLLMENT_ID = "extra:EnrollmentId";
     private static final String EXTRA_ARGUMENTS = "extra:Arguments";
@@ -48,10 +36,6 @@ public class EnrollmentDateFragment extends DataEntryFragment<EnrollmentDateFrag
     private boolean edit;
     private boolean editableRows;
     private EnrollmentDateFragmentForm mForm;
-    private static final String EMPTY_FIELD = "";
-    private static final String DATE_FORMAT = "YYYY-MM-dd";
-    private View enrollmentDatePicker;
-    private View incidentDatePicker;
     private SaveThread saveThread;
 
 
@@ -87,12 +71,6 @@ public class EnrollmentDateFragment extends DataEntryFragment<EnrollmentDateFrag
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        enrollmentDatePicker = LayoutInflater.from(getActivity())
-                .inflate(org.hisp.dhis.android.sdk.R.layout.fragment_data_entry_date_picker, listView, false);
-        incidentDatePicker = LayoutInflater.from(getActivity())
-                .inflate(org.hisp.dhis.android.sdk.R.layout.fragment_data_entry_date_picker, listView, false);
-        listView.addHeaderView(enrollmentDatePicker);
-        listView.addHeaderView(incidentDatePicker);
     }
 
 
@@ -110,64 +88,25 @@ public class EnrollmentDateFragment extends DataEntryFragment<EnrollmentDateFrag
     }
 
     @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem menuItem) {
         if (menuItem.getItemId() == android.R.id.home) {
-            doBack();
-            return true;
+            getFragmentManager().popBackStack();
         }
         else if (menuItem.getItemId() == org.hisp.dhis.android.sdk.R.id.action_new_event)
         {
 
             if (editableRows) {
-                setEditableDatePickerRows(false);
+                setEditableDataEntryRows(false);
             } else {
-                setEditableDatePickerRows(true);
+                setEditableDataEntryRows(true);
             }
             editableRows= !editableRows;
         }
-
-        return super.onOptionsItemSelected(menuItem);
-    }
-
-
-    @Override
-    public void doBack()
-    {
-        if(edit)
-        {
-            UiUtils.showConfirmDialog(getActivity(),
-                    getString(org.hisp.dhis.android.sdk.R.string.discard), getString(org.hisp.dhis.android.sdk.R.string.discard_confirm_changes),
-                    getString(org.hisp.dhis.android.sdk.R.string.save_and_close),
-                    getString(org.hisp.dhis.android.sdk.R.string.cancel),
-                    getString(org.hisp.dhis.android.sdk.R.string.discard),
-                    new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            save();
-                            onDetach();
-                            getFragmentManager().popBackStack();
-                            DhisController.hasUnSynchronizedDatavalues = true;
-
-                        }
-                    }, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-
-                        }
-                    }, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            onDetach();
-                            getFragmentManager().popBackStack();
-                        }
-                    });
-        }
-        else
-        {
-            onDetach();
-            getFragmentManager().popBackStack();
-        }
+        return true;
     }
 
     public void flagDataChanged(boolean changed)
@@ -178,127 +117,29 @@ public class EnrollmentDateFragment extends DataEntryFragment<EnrollmentDateFrag
 
 
 
-    private void attachEnrollmentDatePicker() {
-        if (mForm != null && isAdded()) {
-            final EditText datePickerEditText = (EditText) enrollmentDatePicker
-                    .findViewById(org.hisp.dhis.android.sdk.R.id.date_picker_edit_text);
-            View.OnClickListener onClearDateListener = new View.OnClickListener() {
-                @Override public void onClick(View v) {
-                    datePickerEditText.setText(EMPTY_FIELD);
-                    mForm.getEnrollment().setDateOfEnrollment(EMPTY_FIELD);
-                }
-            };
-            DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
-                @Override
-                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                    LocalDate date = new LocalDate(year, monthOfYear + 1, dayOfMonth);
-                    String newValue = date.toString(DateUtils.DATE_PATTERN);
-                    datePickerEditText.setText(newValue);
-                    mForm.getEnrollment().setDateOfEnrollment(newValue);
-                    onRowValueChanged(null);
-                }
-            };
-            String reportDateDescription = mForm.getEnrollment().getProgram().getDateOfEnrollmentDescription()== null ?
-                    getString(org.hisp.dhis.android.sdk.R.string.report_date) : mForm.getEnrollment().getProgram().getDateOfEnrollmentDescription();
-            String value = null;
-            if (mForm.getEnrollment() != null && mForm.getEnrollment().getDateOfEnrollment() != null) {
-                DateTime date = DateTime.parse(mForm.getEnrollment().getDateOfEnrollment());
-                String newValue = date.toString(DateUtils.DATE_PATTERN);
-                datePickerEditText.setText(newValue);
-            }
-            setDatePicker(enrollmentDatePicker, reportDateDescription, value, onClearDateListener, onDateSetListener);
-        }
-    }
 
-    private void attachIncidentDatePicker() {
-        if (mForm != null && isAdded()) {
-            final EditText datePickerEditText = (EditText) incidentDatePicker
-                    .findViewById(org.hisp.dhis.android.sdk.R.id.date_picker_edit_text);
-            View.OnClickListener onClearDateListener = new View.OnClickListener() {
-                @Override public void onClick(View v) {
-                    datePickerEditText.setText(EMPTY_FIELD);
-                    mForm.getEnrollment().setDateOfIncident(EMPTY_FIELD);
-                }
-            };
-            DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
-                @Override
-                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                    LocalDate date = new LocalDate(year, monthOfYear + 1, dayOfMonth);
-                    String newValue = date.toString(DateUtils.DATE_PATTERN);
-                    datePickerEditText.setText(newValue);
-                    mForm.getEnrollment().setDateOfIncident(newValue);
-                    onRowValueChanged(null);
-                }
-            };
-            String reportDateDescription = mForm.getEnrollment().getProgram().getDateOfIncidentDescription()== null ?
-                    getString(org.hisp.dhis.android.sdk.R.string.report_date) : mForm.getEnrollment().getProgram().getDateOfIncidentDescription();
-            String value = null;
-            if (mForm.getEnrollment() != null && mForm.getEnrollment().getDateOfIncident() != null) {
-                DateTime date = DateTime.parse(mForm.getEnrollment().getDateOfIncident());
-                value = date.toString(DateUtils.DATE_PATTERN);
-            }
-            setDatePicker(incidentDatePicker, reportDateDescription, value, onClearDateListener, onDateSetListener);
-        }
-    }
 
-    private void setDatePicker(View datePicker, String labelValue, String dateValue, View.OnClickListener clearDateListener, final DatePickerDialog.OnDateSetListener onDateSetListener) {
-        final TextView label = (TextView) datePicker
-                .findViewById(org.hisp.dhis.android.sdk.R.id.text_label);
-        final EditText datePickerEditText = (EditText) datePicker
-                .findViewById(org.hisp.dhis.android.sdk.R.id.date_picker_edit_text);
-        final ImageButton clearDateButton = (ImageButton) datePicker
-                .findViewById(org.hisp.dhis.android.sdk.R.id.clear_edit_text);
-        clearDateButton.setOnClickListener(clearDateListener);
-        datePickerEditText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LocalDate currentDate = new LocalDate();
-                DatePickerDialog picker = new DatePickerDialog(getActivity(),onDateSetListener
-                        , currentDate.getYear(),
-                        currentDate.getMonthOfYear() - 1,
-                        currentDate.getDayOfMonth());
-                picker.getDatePicker().setMaxDate(DateTime.now().getMillis());
-                picker.show();
-            }
-        });
-        label.setText(labelValue);
-        if(dateValue!=null) {
-            datePickerEditText.setText(dateValue);
-        }
-    }
-
-    private void setEditableDatePickerRows(boolean editable)
+    public void setEditableDataEntryRows(boolean editable)
     {
-        //should rather fetch through loader
-
-
-        final TextView enrollmentLabel = (TextView) enrollmentDatePicker
-                .findViewById(org.hisp.dhis.android.sdk.R.id.text_label);
-        final EditText enrollmentDatePickerEditText = (EditText) enrollmentDatePicker
-                .findViewById(org.hisp.dhis.android.sdk.R.id.date_picker_edit_text);
-        final ImageButton enrollmentClearDateButton = (ImageButton) enrollmentDatePicker
-                .findViewById(org.hisp.dhis.android.sdk.R.id.clear_edit_text);
-
-        final TextView incidentLabel = (TextView) incidentDatePicker
-                .findViewById(org.hisp.dhis.android.sdk.R.id.text_label);
-        final EditText incidentDatePickerEditText = (EditText) incidentDatePicker
-                .findViewById(org.hisp.dhis.android.sdk.R.id.date_picker_edit_text);
-        final ImageButton incidentClearDateButton = (ImageButton) incidentDatePicker
-                .findViewById(org.hisp.dhis.android.sdk.R.id.clear_edit_text);
-
-
-        enrollmentDatePickerEditText.setEnabled(editable);
-        enrollmentDatePickerEditText.setClickable(editable);
-        enrollmentLabel.setEnabled(editable);
-        enrollmentLabel.setClickable(editable);
-        enrollmentClearDateButton.setClickable(editable);
-        enrollmentClearDateButton.setEnabled(editable);
-        incidentDatePickerEditText.setEnabled(editable);
-        incidentDatePickerEditText.setClickable(editable);
-        incidentLabel.setEnabled(editable);
-        incidentLabel.setClickable(editable);
-        incidentClearDateButton.setClickable(editable);
-        incidentClearDateButton.setEnabled(editable);
+        List<Row> rows = new ArrayList<>(mForm.getDataEntryRows());
+        listViewAdapter.swapData(null);
+        if(editable)
+        {
+            for(Row row : rows)
+            {
+                row.setEditable(true);
+            }
+        }
+        else
+        {
+            for(Row row : rows)
+            {
+                row.setEditable(false);
+            }
+        }
+        listView.setAdapter(null);
+        listViewAdapter.swapData(rows);
+        listView.setAdapter(listViewAdapter);
     }
 
     @Subscribe
@@ -308,6 +149,7 @@ public class EnrollmentDateFragment extends DataEntryFragment<EnrollmentDateFrag
         if (mForm == null ) {
             return;
         }
+        save();
 
     }
 
@@ -341,9 +183,7 @@ public class EnrollmentDateFragment extends DataEntryFragment<EnrollmentDateFrag
         progressBar.setVisibility(View.GONE);
         listView.setVisibility(View.VISIBLE);
         listViewAdapter.swapData(data.getDataEntryRows());
-        attachEnrollmentDatePicker();
-        attachIncidentDatePicker();
-        setEditableDatePickerRows(false);
+        setEditableDataEntryRows(false);
     }
 
     @Override
--- app/src/main/java/org/hisp/dhis/android/trackercapture/fragments/enrollmentdate/EnrollmentDateFragmentQuery.java
+++ app/src/main/java/org/hisp/dhis/android/trackercapture/fragments/enrollmentdate/EnrollmentDateFragmentQuery.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import org.hisp.dhis.android.sdk.controllers.tracker.TrackerController;
 import org.hisp.dhis.android.sdk.persistence.loaders.Query;
 import org.hisp.dhis.android.sdk.persistence.models.Enrollment;
+import org.hisp.dhis.android.sdk.ui.adapters.rows.dataentry.EnrollmentDatePickerRow;
 import org.hisp.dhis.android.sdk.ui.adapters.rows.dataentry.Row;
 
 import java.util.ArrayList;
@@ -31,7 +32,12 @@ public class EnrollmentDateFragmentQuery implements Query<EnrollmentDateFragment
             return fragmentForm;
 
         List<Row> dataEntryRows = new ArrayList<>();
+        dataEntryRows.add(new EnrollmentDatePickerRow(enrollment.getProgram().getDateOfEnrollmentDescription(), enrollment, enrollment.getDateOfEnrollment(),null));
 
+        if(enrollment.getProgram().getDisplayIncidentDate())
+        {
+            dataEntryRows.add(new EnrollmentDatePickerRow(enrollment.getProgram().getDateOfIncidentDescription(),enrollment,null, enrollment.getDateOfIncident()));
+        }
 
         fragmentForm.setEnrollment(enrollment);
         fragmentForm.setDataEntryRows(dataEntryRows);

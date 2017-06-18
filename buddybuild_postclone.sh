b7bd970568814b9765562235a9ff9b4165e34def
ifoche <nacho.foche@gmail.com>
2017-06-18 03:32:17 +0200

    configure sdk for buddybuild in postclone script

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -5,3 +5,8 @@ gitPath=$(git rev-parse --show-toplevel)
 
 # Generate last commit
 sh ${gitPath}/generate_last_commit.sh
+
+# Use tracker capture SDK branch
+cd sdk
+git checkout tracker-capture
+cd -

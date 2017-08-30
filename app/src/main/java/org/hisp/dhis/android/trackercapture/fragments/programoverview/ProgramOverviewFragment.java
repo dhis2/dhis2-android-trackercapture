a6ac403e43ddd34d745dfe44c76f9047dd599ffd
idelcano <ignaciodelcano@gmail.com>
2017-08-30 19:39:52 +0200

    Fix delete event

--- app/src/main/java/org/hisp/dhis/android/trackercapture/fragments/programoverview/ProgramOverviewFragment.java
+++ app/src/main/java/org/hisp/dhis/android/trackercapture/fragments/programoverview/ProgramOverviewFragment.java
@@ -876,7 +876,13 @@ public class ProgramOverviewFragment extends AbsProgramRuleFragment implements V
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         eventItemRow.setStatus(Event.STATUS_DELETED);
-                        eventItemRow.save();
+                        if(eventItemRow.getCreated()!=null){
+                            eventItemRow.setFromServer(false);
+                            eventItemRow.setDataValues(null);
+                            eventItemRow.save();
+                        }else{
+                            eventItemRow.delete();
+                        }
                         dialog.dismiss();
                     }
                 });
